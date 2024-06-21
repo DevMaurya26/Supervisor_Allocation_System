355c8898aae71bdf8e2d1c2e73c0bb887be438dc
Dextra <devkumarmaurya.it21@scet.ac.in>
2024-06-21 15:46:23 +0530

    change request working from both side admin and Client

--- supervisor/main/models.py
+++ supervisor/main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 class Allocation_File(models.Model):
     Created_at = models.DateTimeField(auto_now_add=True,)
@@ -11,5 +12,14 @@ class Allocation_File(models.Model):
     def __str__(self):
         return str(self.file_name)
     
+class Change_reqs(models.Model):
+    requestor_id  = models.ForeignKey(User,on_delete=models.CASCADE)
+    reqtor_name = models.CharField(max_length=100)
+    file_name = models.CharField(max_length=400)
+    previous_details = models.CharField(max_length=300)
+    college = models.CharField(max_length=250)
+    req_date = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(default=0)
+
 
 
--- supervisor/main/urls.py
+++ supervisor/main/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('create', views.create, name='create'),
     path('allocation/', views.generated, name='list'),
     path('file/<str:file_name>', views.download, name='file_res'),
-
+    path('change_request/<str:data>',views.change_req, name='change_req'),
+    path('your_change_req',views.user_change_req,name='your_change_req'),
+    path('admin_decision/<int:decision>/<req_id>',views.admin_aprove_reject,name='admin_decision')
 ]
