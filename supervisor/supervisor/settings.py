48858ddcc61fab92a6bcb5322f7566ae5d935bb6
Dev Maurya <155285196+DevMaurya26@users.noreply.github.com>
2024-06-23 21:24:42 +0530

    Update settings.py

--- supervisor/supervisor/settings.py
+++ supervisor/supervisor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ald*(*3yzy_fg1&&nk!h0b9-qw=+++9p9lfx2g-0ux^18yb1h&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://supervisor-allocation-system.onrender.com']
 
 
 # Application definition
